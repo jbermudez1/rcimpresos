684f06a03e14e713b4dcc2963138ffb167959f97
Javier Bermudez <jbermudez@proyeccione.com>
2017-06-05 19:37:42 -0600

    Correcciones de estios y mapa
    Se agrego el mapa

--- public/js/index.js
+++ public/js/index.js
@@ -29,23 +29,66 @@ var _mobileMenu2 = _interopRequireDefault(_mobileMenu);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+$(window).scroll(function (event) {
+	var scroll = $(window).scrollTop();
+	console.log(scroll);
+});
+
 $(document).ready(function () {
 	_mobileMenu2.default.events();
 	console.log('Document Ready');
 	$('.carousel.carousel-slider').carousel({ fullWidth: true });
+
+	var section = $('body').data('section');
+	console.log(section);
+
+	if (section == 'index') {
+		initMap();
+		$('#section-menu li').removeClass('selected');
+		$('#inicio').addClass('selected');
+	}
+	if (section == 'nosotros') {
+		$('#section-menu li').removeClass('selected');
+		$('#nosotros').addClass('selected');
+	}
+	if (section == 'servicios') {
+		$('#section-menu li').removeClass('selected');
+		$('#servicios').addClass('selected');
+	}
+	if (section == 'cotizador') {
+		$('#section-menu li').removeClass('selected');
+		$('#cotizador').addClass('selected');
+	}
+	if (section == 'contacto') {
+		$('#section-menu li').removeClass('selected');
+		$('#contacto').addClass('selected');
+	}
 });
 
-$(window).scroll(function (event) {
-	var scroll = $(window).scrollTop();
-	console.log(scroll);
-	// if(scroll >150) {
-	// 	$('.haderLarge .container .top').addClass('hide')
-	// 	$('.haderLarge .container .bottom').addClass('statis')
-	// } else {
-	// 	$('.haderLarge .container .top').removeClass('hide')
-	// 	$('.haderLarge .container .bottom').removeClass('statis')
-	// }
-});
+function initMap() {
+	var myLatLng = { lat: 24.8000966, lng: -107.3977691 };
+
+	console.log('iniciando el mapa');
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: myLatLng
+	});
+
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: 'RC impresos'
+	});
+	marker.addListener('click', function () {
+		infowindow.open(map, marker);
+	});
+	var contentString = ' <h5> RC IMPRESOS </h5><p>José Aguilar Barraza No. 126 Pte.Col. Jorge Almada. C.P 80200 </p>';
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString,
+		maxWidth: 300
+	});
+}
 
 },{"./components/mobile-menu":1}]},{},[2])
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,67 @@
 import mobilMenu from './components/mobile-menu'
 
+$(window).scroll( (event)=>{
+	let scroll = $(window).scrollTop()
+	console.log(scroll)
+})
+
 $(document).ready(()=>{
 	mobilMenu.events()
 	console.log('Document Ready')
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
 
+	let section = $('body').data('section')
+	console.log(section)
+
+	if(section == 'index'){
+		initMap();
+		$('#section-menu li').removeClass('selected')
+		$('#inicio').addClass('selected')
+	}
+	if(section == 'nosotros'){
+		$('#section-menu li').removeClass('selected')
+		$('#nosotros').addClass('selected')
+	}
+	if(section == 'servicios'){
+		$('#section-menu li').removeClass('selected')
+		$('#servicios').addClass('selected')
+	}
+	if(section == 'cotizador'){
+		$('#section-menu li').removeClass('selected')
+		$('#cotizador').addClass('selected')
+	}
+	if(section == 'contacto'){
+		$('#section-menu li').removeClass('selected')
+		$('#contacto').addClass('selected')
+	}
+
+	
+
 })
 
+function initMap () {
+	var myLatLng = {lat: 24.8000966, lng: -107.3977691};
 
-$(window).scroll( (event)=>{
-	let scroll = $(window).scrollTop()
-	console.log(scroll)
-	// if(scroll >150) {
-	// 	$('.haderLarge .container .top').addClass('hide')
-	// 	$('.haderLarge .container .bottom').addClass('statis')
-	// } else {
-	// 	$('.haderLarge .container .top').removeClass('hide')
-	// 	$('.haderLarge .container .bottom').removeClass('statis')
-	// }
-})
+	console.log('iniciando el mapa')
+	var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+	var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'RC impresos'
+        });
+	marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+	var contentString = ' <h5> RC IMPRESOS </h5><p>José Aguilar Barraza No. 126 Pte.Col. Jorge Almada. C.P 80200 </p>'
+
+	var infowindow = new google.maps.InfoWindow({
+          content: contentString,
+          maxWidth: 300
+        });
+
+
+}
